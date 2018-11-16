1c011f544d02fbbd6716ba2be51d88ca6a473b11
Santosh Panda <skumary2k@gmail.com>
2018-11-15 20:56:22 -0500

    Environment file modified.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyBBi9YVpbIR28096YNyqNOyj6lQTxYwQ-A",
-    authDomain: "bookland-store.firebaseapp.com",
-    databaseURL: "https://bookland-store.firebaseio.com",
-    projectId: "bookland-store",
-    storageBucket: "bookland-store.appspot.com",
-    messagingSenderId: "980941906148"
-  }
+  
 };
