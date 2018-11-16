2a18613908a8462d091d4f6c2ce629eb42c0f413
Santosh Panda <skumary2k@gmail.com>
2018-11-15 20:54:02 -0500

    Firebase connection added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBBi9YVpbIR28096YNyqNOyj6lQTxYwQ-A",
+    authDomain: "bookland-store.firebaseapp.com",
+    databaseURL: "https://bookland-store.firebaseio.com",
+    projectId: "bookland-store",
+    storageBucket: "bookland-store.appspot.com",
+    messagingSenderId: "980941906148"
+  }
 };
